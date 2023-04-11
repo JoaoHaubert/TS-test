3f522ff23117bd96c2168965a1071d583660c8a1
João Haubert <jhb.haubert@gmail.com>
2023-04-11 16:55:37 -0400

    check if the value exists

--- narrowing/index.js
+++ narrowing/index.js
@@ -14,3 +14,20 @@ function sum(a, b) {
 sum("23", "50");
 sum(34, 50);
 sum("32", 90);
+//Checkin if value exists
+function operations(arr, operation) {
+    if (operation === "sum") {
+        const sum = arr.reduce((i, total) => i + total);
+        console.log(sum);
+    }
+    else if (operation === "multiply") {
+        const multiply = arr.reduce((i, total) => i * total);
+        console.log(multiply);
+    }
+    else {
+        console.log("O sistema não superta este tipo de operação");
+    }
+}
+operations([10, 20, 30], "sum");
+operations([10, 5, 5], "multiply");
+operations([1, 2, 3], "division");
--- narrowing/index.ts
+++ narrowing/index.ts
@@ -12,3 +12,21 @@ function sum(a: number | string, b: number | string) {
 sum("23", "50")
 sum(34, 50)
 sum("32", 90)
+
+//Checkin if value exists
+function operations(arr: number[], operation?: string | undefined) {
+    if(operation === "sum") {
+        const sum = arr.reduce((i, total) => i + total)
+        console.log(sum)
+        
+    } else if (operation === "multiply") {
+        const multiply = arr.reduce((i, total) => i * total)
+        console.log(multiply)
+    } else {
+        console.log("O sistema não superta este tipo de operação")
+    }
+} 
+
+operations([10,20,30], "sum")
+operations([10,5,5], "multiply")
+operations([1,2,3], "division")
