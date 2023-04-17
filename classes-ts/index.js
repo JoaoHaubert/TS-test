24d50438b1b50e7412722108ca206f8af6276531
João Haubert <jhb.haubert@gmail.com>
2023-04-17 10:28:05 -0400

    Campos readonly

--- classes-ts/index.js
+++ classes-ts/index.js
@@ -16,3 +16,16 @@ class Shoes {
 const nike = new Shoes("Nike", 9.5);
 console.log(nike);
 console.log(nike.brand);
+//Campos readonly
+class SkateBoard {
+    constructor(shape) {
+        this.wheels = 4;
+        this.shape = shape;
+    }
+}
+const skate = new SkateBoard("Zero");
+console.log(skate);
+console.log(skate.shape);
+console.log(skate.wheels);
+skate.shape = "Flip";
+//skate.wheels = 5 **Erro, o readonly ja deixa lockado o numero 4, nao tem como alterar**
--- classes-ts/index.ts
+++ classes-ts/index.ts
@@ -27,3 +27,22 @@ const nike = new Shoes("Nike", 9.5)
 
 console.log(nike)
 console.log(nike.brand)
+
+//Campos readonly
+class SkateBoard {
+    shape
+    readonly wheels = 4
+
+    constructor(shape: string) {
+        this.shape = shape
+    }
+}
+
+const skate = new SkateBoard("Zero")
+
+console.log(skate)
+console.log(skate.shape)
+console.log(skate.wheels)
+
+skate.shape = "Flip"
+//skate.wheels = 5 **Erro, o readonly ja deixa lockado o numero 4, nao tem como alterar**
