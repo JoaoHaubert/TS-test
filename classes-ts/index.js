2aec4d2e0d1cd01717c3488118f6736bf922d7cf
João Haubert <jhb.haubert@gmail.com>
2023-04-17 10:12:19 -0400

    iniciando classes sem valores

---
+++ classes-ts/index.js
@@ -0,0 +1,8 @@
+"use strict";
+//Iniciando classe sem valores
+class Bus {
+}
+const marcopolo = new Bus();
+console.log(marcopolo);
+marcopolo.brand = "Marcopolo";
+console.log(marcopolo);
---
+++ classes-ts/index.ts
@@ -0,0 +1,13 @@
+//Iniciando classe sem valores
+class Bus {
+    brand!: string
+    age!: number
+}
+
+const marcopolo = new Bus()
+
+console.log(marcopolo)
+
+marcopolo.brand = "Marcopolo"
+
+console.log(marcopolo)
