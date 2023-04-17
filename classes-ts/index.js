2aea69a21ef2515b8698112e4ff3b3cb7cf6db6d
João Haubert <jhb.haubert@gmail.com>
2023-04-17 10:22:34 -0400

    Constructor

--- classes-ts/index.js
+++ classes-ts/index.js
@@ -6,3 +6,13 @@ const marcopolo = new Bus();
 console.log(marcopolo);
 marcopolo.brand = "Marcopolo";
 console.log(marcopolo);
+//Constructor
+class Shoes {
+    constructor(brand, size) {
+        this.brand = brand;
+        this.size = size;
+    }
+}
+const nike = new Shoes("Nike", 9.5);
+console.log(nike);
+console.log(nike.brand);
--- classes-ts/index.ts
+++ classes-ts/index.ts
@@ -11,3 +11,19 @@ console.log(marcopolo)
 marcopolo.brand = "Marcopolo"
 
 console.log(marcopolo)
+
+//Constructor
+class Shoes {
+    brand
+    size
+
+    constructor(brand: string, size: number) {
+        this.brand = brand
+        this.size = size
+    }
+}
+
+const nike = new Shoes("Nike", 9.5)
+
+console.log(nike)
+console.log(nike.brand)
