20a336f4a34b43ea020f80874cb8c4a3b08fa23d
João Haubert <jhb.haubert@gmail.com>
2023-04-18 10:48:56 -0400

    Al

--- modulos-ts/index.js
+++ modulos-ts/index.js
@@ -3,12 +3,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const car_1 = __importDefault(require("./car"));
-const varengine_1 = require("./varengine");
-const multimports_1 = require("./multimports");
 //Importando arquivos
+const car_1 = __importDefault(require("./car"));
 (0, car_1.default)();
 //Importando variaveis
+const varengine_1 = require("./varengine");
 console.log(varengine_1.engine);
 //Multiplos imports
+const multimports_1 = require("./multimports");
+console.log(multimports_1.year);
+console.log(multimports_1.hp);
 (0, multimports_1.showDodge)();
--- modulos-ts/index.ts
+++ modulos-ts/index.ts
@@ -1,14 +1,17 @@
-import importCar from "./car";
-import { engine } from "./varengine";
-import { year, hp, showDodge } from "./multimports";
-
 //Importando arquivos
+import importCar from "./car";
 importCar()
 
 //Importando variaveis
+import { engine } from "./varengine";
 console.log(engine)
 
 //Multiplos imports
+import { year, hp, showDodge } from "./multimports";
 console.log(year)
 console.log(hp)
 showDodge()
+
+//Alias para import
+import { nome as n } from "./change";
+console.log(n)
