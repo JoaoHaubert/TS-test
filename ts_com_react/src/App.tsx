ebe8211bdb952636412971a1cd82ca17314a58fa
João Haubert <jhb.haubert@gmail.com>
2023-05-04 10:48:34 -0300

    react variables

--- ts_com_react/src/App.tsx
+++ ts_com_react/src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import './App.css';
+
 
 function App() {
+  //Variaveis
+  const name: string = "Ayla"
+  const age: number = 1.7
+  const isRunning: boolean = true
+
   return (
     <div className="App">
       <h1>React tsx</h1>
+      <p>Name: {name}</p>
+      <p>Age: {age}</p>
+      {isRunning && 
+        <p>She is running!</p>
+      }
     </div>
   );
 }
